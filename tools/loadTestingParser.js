d5a4b8b636bacc702dccef5b9f7419c9d66e17ae
Miguel de Barros <miguel@debarros.me>
2018-12-21 17:04:36 +0200

    Updated loadTesting script to include mean and deviation, etc

--- tools/loadTestingParser.js
+++ tools/loadTestingParser.js
@@ -30,6 +30,9 @@
  --------------
  ******/
 
+// const p = '%{TIMESTAMP_ISO8601:timestamp} - %{DATA:specialCharacters}: guid=%{UUID:uuid} - %{GREEDYDATA:process}'
+// const p = '%{TIMESTAMP_ISO8601:timestamp} - %{GREEDYDATA:specialCharacters}: guid=%{UUID:uuid} - %{GREEDYDATA:process}'
+// const p = '%{TIMESTAMP_ISO8601:timestamp} - %{DATA:specialCharacters}cid=%{GREEDYDATA:uuid}, fsp=%{GREEDYDATA:fsp}, source=%{GREEDYDATA:source}, dest=%{GREEDYDATA:dest}] ~ %{GREEDYDATA:process}'
 const p = '%{TIMESTAMP_ISO8601:timestamp} - %{DATA:specialCharacters}: guid=%{GREEDYDATA:uuid} - %{GREEDYDATA:process}'
 const nodeGrok = require('node-grok')
 const _ = require('lodash')
@@ -53,6 +56,9 @@ let firstLine
 let lastLine
 let perEntryResponse = []
 let lineCount = 0
+let transfersThatTakeLongerThanASecond = 0
+let beginTime = new Date().getTime()
+let longestTransaction
 
 function compare (a, b) {
   const timestampA = a.timestamp
@@ -95,6 +101,22 @@ lr.on('line', function (line) {
       if (entry.entries.length === parseInt(argv.num)) {
         entry.totalDifference = new Date(entry.entries[entry.entries.length - 1].timestamp).getTime() - new Date(entry.entries[0].timestamp).getTime()
         perEntryResponse.push(entry.totalDifference)
+        if (entry.totalDifference >= 1000) {
+          transfersThatTakeLongerThanASecond++
+        }
+        if (longestTransaction) {
+          if (longestTransaction.entry.totalDifference < entry.totalDifference) {
+            longestTransaction = {
+              transferId: entry.entries[0].uuid,
+              entry
+            }
+          }
+        } else {
+          longestTransaction = {
+            transferId: entry.entries[0].uuid,
+            entry
+          }
+        }
       }
       logMap[logLine.uuid] = entry
     }
@@ -103,24 +125,34 @@ lr.on('line', function (line) {
 })
 
 lr.on('end', function () {
+  const mean = perEntryResponse.reduce((a, b) => a + b) / perEntryResponse.length
+  let differenceFromMeanSquared = []
+  for (let entry of perEntryResponse) {
+    differenceFromMeanSquared.push(Math.pow((entry - mean), 2))
+  }
+  let variance = differenceFromMeanSquared.reduce((a, b) => a + b) / differenceFromMeanSquared.length
+  let standardDeviation = Math.sqrt(variance)
   const firstTime = new Date(firstLine.timestamp).getTime()
   const lastTime = new Date(lastLine.timestamp).getTime()
-  const totalTime = (lastTime - firstTime) / 1000
+  const totalTime = (lastTime - firstTime)
   const totalTransactions = perEntryResponse.length
   const sortedPerEntryResponse = perEntryResponse.sort(compareNumbers)
-  const shortestResponse = sortedPerEntryResponse[0] / 1000
-  const longestResponse = sortedPerEntryResponse[perEntryResponse.length - 1] / 1000
-  const averageTransaction = (perEntryResponse.reduce((a, b) => a + b, 0) / totalTransactions) / 1000
+  const shortestResponse = sortedPerEntryResponse[0]
+  const longestResponse = sortedPerEntryResponse[perEntryResponse.length - 1]
 
   console.log('First request: ' + firstLine.timestamp)
   console.log('Last request: ' + lastLine.timestamp)
   console.log('Total number of lines in log file: ' + lineCount)
   console.log('Number of unique matched entries: ' + totalTransactions)
-  // console.log('First request duration in milliseconds ' + new Date(firstLine.timestamp).getTime())
-  // console.log('Last request duration in milliseconds ' + new Date(lastLine.timestamp).getTime())
-  console.log('Total difference of all requests in seconds: ' + (totalTime))
-  console.log('Shortest response time in second: ' + shortestResponse)
-  console.log('Longest response time in second: ' + longestResponse)
-  console.log('The average transaction in second: ' + averageTransaction)
-  console.log('Average transactions per second: ' + (totalTransactions / totalTime))
+  console.log('Total difference of all requests in milliseconds: ' + (totalTime))
+  console.log('Shortest response time in millisecond: ' + shortestResponse)
+  console.log('Longest response time in millisecond: ' + longestResponse)
+  console.log('Mean/The average time a transaction takes in millisecond: ' + mean)
+  console.log('Variance in milliseconds: ' + variance)
+  console.log('Standard deviation in milliseconds: ' + standardDeviation)
+  console.log(`The longest transaction was: ${longestTransaction.transferId} with a time of ${longestTransaction.entry.totalDifference} milliseconds`)
+  console.log('Number of entries that took longer than a second: ' + transfersThatTakeLongerThanASecond)
+  console.log(`% of entries that took longer than a second: ${(transfersThatTakeLongerThanASecond / totalTransactions * 100).toFixed(2)}%`)
+  console.log('Average transactions per second: ' + (totalTransactions / (totalTime / 1000)))
+  console.log('Total time that script takes to run in seconds: ' + (new Date().getTime() - beginTime) / 1000)
 })
